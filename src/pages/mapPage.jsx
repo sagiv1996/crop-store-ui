f2b853d6a6d3ae94fee2082639df24d47466240d
sagiv bonali <sagivb13@gmail.com>
2023-07-03 17:08:29 +0300

    add space for screen

--- src/pages/mapPage.jsx
+++ src/pages/mapPage.jsx
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom";
 import MapDisplay from "../components/MapDisplay";
+import ComboboxLocation from "../components/ComboxLocation";
 
 const MapPage = () => {
   const { lat, lng } = useParams();
   const center = { lat: Number(lat), lng: Number(lng) };
 
-  return <MapDisplay center={center} />;
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-3/12 border md:border-r-8 overflow-x-auto md:overflow-visible">
+        <div className="border p-4 rounded">
+          <ComboboxLocation />
+        </div>
+        <ul className="flex md:flex-col overflow-x-auto"></ul>
+      </div>
+      <div className="w-full md:w-9/12">
+        <MapDisplay center={center} />
+      </div>
+    </div>
+  );
 };
 
 export default MapPage;
