ae28a66d8cf2206f5e4f20ca405e6f3757450674
sagiv bonali <sagivb13@gmail.com>
2023-07-03 18:55:53 +0300

    use with @tanstack/react-query libary

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MapPage from "./pages/MapPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/map/:lat/:lng" element={<MapPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/map/:lat/:lng"
+            element={<MapPage />}
+            lazy={true}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/mapPage.jsx
+++ src/pages/mapPage.jsx
@@ -1,34 +1,66 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import MapDisplay from "../components/MapDisplay";
 import ComboboxLocation from "../components/ComboxLocation";
 import { GraphQLClient } from "graphql-request";
 import { getStores } from "../graphqlQueries";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
 const MapPage = () => {
   const { lat, lng } = useParams();
+  const { data, isLoading } = useQuery({
+    queryKey: ["getStores", lat, lng],
+    queryFn: async () =>
+      await client.request(getStores, { lat: Number(lat), lng: Number(lng) }),
+    enabled: !!lat && !!lng,
+  });
+
   const center = { lat: Number(lat), lng: Number(lng) };
-  console.log(`${import.meta.env.VITE_BACKEND_ENDPOINT}graphql`);
   const client = new GraphQLClient(
     `${import.meta.env.VITE_BACKEND_ENDPOINT}graphql`
   );
 
-  useEffect(() => {
-    client
-      .request(getStores, { lat: Number(lat), lng: Number(lng) })
-      .then((res) => console.log({ res }));
-  }, [lat, lng]);
-
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="w-full md:w-3/12 border md:border-r-8 overflow-x-auto md:overflow-visible">
-        <div className="border p-4 rounded">
-          <ComboboxLocation />
+    <div>
+      {isLoading ? (
+        <div>Loading.....</div>
+      ) : (
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-3/12 border md:border-r-8 overflow-x-auto md:overflow-visible">
+            <div className="border p-4 rounded">
+              <ComboboxLocation />
+            </div>
+            <ul className="flex md:flex-col overflow-x-auto">
+              {data &&
+                data.stores.data.map((cropStore) => (
+                  <li
+                    key={cropStore.attributes.name}
+                    className="flex-none  border p-4 rounded"
+                  >
+                    <h3 className="text-xl font-bold">
+                      {cropStore.attributes.name}
+                    </h3>
+                    <h3 className="text-gray-600">
+                      Distance: {cropStore.attributes.distance} km
+                    </h3>
+                    <Link
+                      to={`/map/${cropStore.attributes.location.lat}/${cropStore.attributes.location.lng}`}
+                      className="text-blue-500 hover:text-blue-600"
+                      onClick={() =>
+                        setOpenDialog(cropStore.attributes.location)
+                      }
+                    >
+                      View on Map
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <div className="w-full md:w-9/12">
+            <MapDisplay center={center} />
+          </div>
         </div>
-        <ul className="flex md:flex-col overflow-x-auto"></ul>
-      </div>
-      <div className="w-full md:w-9/12">
-        <MapDisplay center={center} />
-      </div>
+      )}
     </div>
   );
 };
