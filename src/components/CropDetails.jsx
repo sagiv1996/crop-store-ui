86b380246f6afc57600a4bc3842d1de2951b3004
sagiv bonali <sagivb13@gmail.com>
2023-07-06 09:56:42 +0300

    check if have error + set default image

--- src/components/CropDetails.jsx
+++ src/components/CropDetails.jsx
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Dialog } from "@headlessui/react";
 import { FaPhone } from "react-icons/fa";
 import { GraphQLClient } from "graphql-request";
 import { getStoreById } from "../graphqlQueries";
 import { useQuery } from "@tanstack/react-query";
+import defaultImage from "../assets/defaultImage.png";
 
 const url = import.meta.env.VITE_BACKEND_ENDPOINT;
 const client = new GraphQLClient(`${url}/graphql`);
 
 const CropDetails = ({ id }) => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: [`getStoreid`, id],
     queryFn: async () => await client.request(getStoreById, { id }),
     enabled: !!id,
   });
 
   const today = new Date().toLocaleString("en-us", { weekday: "long" });
+
+  const getImageUrl = (record) => {
+    try {
+      return url + record.store.data.attributes.image.data.attributes?.url;
+    } catch (e) {
+      return defaultImage;
+    }
+  };
 
   const isOpenNow = (day) => {
     /**
@@ -36,61 +45,59 @@ const CropDetails = ({ id }) => {
     return now > timeOpen && now < timeClose;
   };
 
+  if (isError) {
+    return "An error occurred";
+  } else if (isLoading) {
+    return "Loading...";
+  }
   return (
     <div>
-      {!isLoading && (
-        <>
-          <Dialog.Title
-            as="h3"
-            className="text-xl font-medium leading-6 text-gray-900"
-          >
-            {data.store.data.attributes?.name}
-          </Dialog.Title>
-          <Dialog.Title
-            as="h4"
-            className="text-lg font-semibold leading-7 text-gray-700"
-          >
-            Address: {data.store.data.attributes?.address}
-          </Dialog.Title>
-          <Dialog.Description className="flex items-center space-x-2">
-            <span className="text-gray-600">
-              {data.store.data.attributes?.phoneNumber}
-            </span>
-            <a
-              className="text-blue-500 hover:text-blue-600"
-              href={`tel:${data.store.data.attributes?.phoneNumber}`}
-            >
-              <FaPhone
-                size={20}
-                title={`Call to ${data.store.data.attributes?.phoneNumber}`}
-              />
-            </a>
-          </Dialog.Description>
-          <ul className="list-disc pl-4">
-            {Object.entries(data.store.data.attributes.openingHours).map(
-              ([day, hours], index) => (
-                <li
-                  key={index}
-                  className={
-                    isOpenNow(day)
-                      ? "text-green-500"
-                      : today === day
-                      ? "text-red-500"
-                      : "text-grey-500"
-                  }
-                >
-                  <span className="font-semibold mr-1">{day}</span>
-                  <span>{`${hours.open} - ${hours.close}`}</span>
-                </li>
-              )
-            )}
-          </ul>
-          <img
-            src={`${url}${data.store.data.attributes.image.data.attributes.url}`}
-            alt="Crop Image"
+      <Dialog.Title
+        as="h3"
+        className="text-xl font-medium leading-6 text-gray-900"
+      >
+        {data.store.data.attributes?.name}
+      </Dialog.Title>
+      <Dialog.Title
+        as="h4"
+        className="text-lg font-semibold leading-7 text-gray-700"
+      >
+        Address: {data.store.data.attributes?.address}
+      </Dialog.Title>
+      <Dialog.Description className="flex items-center space-x-2">
+        <span className="text-gray-600">
+          {data.store.data.attributes?.phoneNumber}
+        </span>
+        <a
+          className="text-blue-500 hover:text-blue-600"
+          href={`tel:${data.store.data.attributes?.phoneNumber}`}
+        >
+          <FaPhone
+            size={20}
+            title={`Call to ${data.store.data.attributes?.phoneNumber}`}
           />
-        </>
-      )}
+        </a>
+      </Dialog.Description>
+      <ul className="list-disc pl-4">
+        {Object.entries(data.store.data.attributes.openingHours).map(
+          ([day, hours], index) => (
+            <li
+              key={index}
+              className={
+                isOpenNow(day)
+                  ? "text-green-500"
+                  : today === day
+                  ? "text-red-500"
+                  : "text-grey-500"
+              }
+            >
+              <span className="font-semibold mr-1">{day}</span>
+              <span>{`${hours.open} - ${hours.close}`}</span>
+            </li>
+          )
+        )}
+      </ul>
+      <img src={getImageUrl(data)} alt="Crop Image" />
     </div>
   );
 };
--- src/pages/MapRoute.jsx
+++ src/pages/MapRoute.jsx
@@ -4,7 +4,6 @@ import {
   useSearchParams,
   useNavigate,
 } from "react-router-dom";
-import { useState } from "react";
 import { GraphQLClient } from "graphql-request";
 import { useQuery } from "@tanstack/react-query";
 import MapDisplay from "../components/MapDisplay";
@@ -27,60 +26,57 @@ const MapRoute = () => {
     `${import.meta.env.VITE_BACKEND_ENDPOINT}/graphql`
   );
 
-  const { data, isLoading } = useQuery(["getStores", lat, lng], () =>
+  const { data, isLoading, isError } = useQuery(["getStores", lat, lng], () =>
     client.request(getStores, { lat: Number(lat), lng: Number(lng) })
   );
 
   const center = { lat: Number(lat), lng: Number(lng) };
 
+  if (isError) {
+    return "An error occurred";
+  } else if (isLoading) {
+    return "Loading...";
+  }
+
   return (
-    <div>
-      {isLoading ? (
-        <div>Loading.....</div>
-      ) : (
-        <div className="flex flex-col md:flex-row">
-          <div className="w-full md:w-3/12 border md:border-r-8 overflow-x-auto md:overflow-visible">
-            <div className="border p-4 rounded">
-              <ComboboxLocation
-                defaultValue={address}
-                onChange={handleChange}
-              />
-            </div>
-            <ul className="flex md:flex-col overflow-x-auto">
-              {data &&
-                data.stores.data.map((cropStore, index) => (
-                  <li
-                    key={cropStore.attributes.name}
-                    className="flex-none border p-4 rounded"
-                  >
-                    <h3 className="text-xl font-bold">
-                      {`${index + 1}`} - {cropStore.attributes.name}
-                    </h3>
-                    <h3 className="text-gray-600">
-                      Distance: {cropStore.attributes.distance} km
-                    </h3>
-                    <Link
-                      to={{
-                        path: `/map/${cropStore.attributes.location.lat}/${cropStore.attributes.location.lng}`,
-                        search: `cropStoreId=${cropStore.id}`,
-                      }}
-                      className="text-blue-500 hover:text-blue-600"
-                    >
-                      View on Map
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <div className="w-full md:w-9/12">
-            <MapDisplay
-              center={center}
-              cropStores={data.stores.data}
-              cropStoreId={cropStoreId}
-            />
-          </div>
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-3/12 border md:border-r-8 overflow-x-auto md:overflow-visible">
+        <div className="border p-4 rounded">
+          <ComboboxLocation defaultValue={address} onChange={handleChange} />
         </div>
-      )}
+        <ul className="flex md:flex-col overflow-x-auto">
+          {data &&
+            data.stores.data.map((cropStore, index) => (
+              <li
+                key={cropStore.attributes.name}
+                className="flex-none border p-4 rounded"
+              >
+                <h3 className="text-xl font-bold">
+                  {`${index + 1}`} - {cropStore.attributes.name}
+                </h3>
+                <h3 className="text-gray-600">
+                  Distance: {cropStore.attributes.distance} km
+                </h3>
+                <Link
+                  to={{
+                    path: `/map/${cropStore.attributes.location.lat}/${cropStore.attributes.location.lng}`,
+                    search: `cropStoreId=${cropStore.id}`,
+                  }}
+                  className="text-blue-500 hover:text-blue-600"
+                >
+                  View on Map
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="w-full md:w-9/12">
+        <MapDisplay
+          center={center}
+          cropStores={data.stores.data}
+          cropStoreId={cropStoreId}
+        />
+      </div>
     </div>
   );
 };
